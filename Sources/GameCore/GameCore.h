20ec62cff3e40132746fd323d433bbdba493072e
yuuxuu1308\kimura <kimura.yuu1120@gmail.com>
2025-06-09 23:54:06 +0900

    ・GameCoreクラスの追加

---
+++ Sources/GameCore/GameCore.h
@@ -0,0 +1,30 @@
+﻿/**
+ * @file GameCore.h
+ * @brief
+ * @author Yu Kimura
+ * @date 2025/06/03
+ */
+
+#pragma once
+
+#include <Windows.h>
+
+namespace MyGameCore
+{
+	class GameCore
+	{
+	private:
+		GameCore();
+		~GameCore();
+
+	public:
+		static GameCore Instance()
+		{
+			static GameCore gameCore;
+			return gameCore;
+		}
+
+		bool Initialize();
+		void Finalize();
+	};
+} // namespace
