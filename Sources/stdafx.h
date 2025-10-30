8761990476bcd5e7d0b24101af7ef79408423f96
yuuxuu1308\kimura <kimura.yuu1120@gmail.com>
2025-10-30 22:45:21 +0900

    プリコンパイルヘッダーの追加

---
+++ Sources/stdafx.h
@@ -0,0 +1,31 @@
+﻿/**
+ * @file stdafx.h
+ * @brief
+ * @author Yu Kimura
+ * @date 2021/04/09
+ */
+
+#pragma once
+
+// windows
+#include <windows.h>
+#include <stdio.h>
+#include <winnt.h>
+
+// std
+#include <vector>
+#include <array>
+#include <list>
+#include <map>
+#include <queue>
+#include <string>
+#include <regex>
+#include <functional>
+#include <thread>
+#include <mutex>
+#include <random>
+
+// FI/FO
+#include <iostream>
+#include <fstream>
+#include <sstream>
